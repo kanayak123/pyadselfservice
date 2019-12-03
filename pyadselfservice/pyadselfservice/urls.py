8cae0cc9e46845f4804a90554d0910cf64122ea6
Amith Nayak <kanayak123@users.noreply.github.com>
2019-12-03 12:35:53 +0530

    fixed compatibility with django 3.0

--- pyadselfservice/pyadselfservice/urls.py
+++ pyadselfservice/pyadselfservice/urls.py
@@ -28,7 +28,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.contrib.auth.views import login
+#from django.contrib.auth.views import login
 
 from validateuser import views
 
